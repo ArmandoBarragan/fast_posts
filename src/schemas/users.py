bac52ca725288e20cbecedcb4c55792c2c16be01
Armando <armandobp765@gmail.com>
2022-10-27 09:06:41 -0600

    added login schema and password and email mixins

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -2,16 +2,27 @@ from pydantic import BaseModel
 from pydantic import Field
 
 
-class UserMixin(BaseModel):
-    first_name: str = Field(..., min_length=2, max_length=30)
-    last_name: str = Field(..., min_length=2, max_length=50)
+class EmailMixin(BaseModel):
     email: str = Field(..., min_length=2, max_length=50)
 
 
-class CreateAccountSchema(UserMixin):
+class PasswordMixin(BaseModel):
     password: str = Field(..., min_length=8, max_length=60)
+
+
+class UserMixin(EmailMixin):
+    first_name: str = Field(..., min_length=2, max_length=30)
+    last_name: str = Field(..., min_length=2, max_length=50)
+
+
+class CreateAccountSchema(UserMixin, PasswordMixin):
     class Config:
         orm_mode=True
 
+
 class ReturnUserSchema(UserMixin):
     id: int
+
+
+class LoginSchema(EmailMixin, PasswordMixin):
+    pass
